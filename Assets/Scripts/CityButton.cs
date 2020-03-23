ae6bf1bccf979024b237987e0590cc5d414e331d
Dmitry <42086502+EncodingDeus@users.noreply.github.com>
2020-03-24 03:04:13 +0500

    Added search by city

---
+++ Assets/Scripts/CityButton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MyWeather
+{
+    public class CityButton : MonoBehaviour
+    {
+        [SerializeField]
+        private Text buttonText;
+
+        private int id;
+        private string name;
+
+        public void Show(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+
+            buttonText.text = name;
+            gameObject.SetActive(true);
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void OnClick()
+        {
+            Debug.Log($"id:{id} name:{name}");
+        }
+    }
+}
--- Assets/Scripts/MainApp.cs
+++ Assets/Scripts/MainApp.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using System.Net;
 using System.IO;
 using UnityEngine.UI;
-using TMPro;
 using System.Data;
+using System;
 
 namespace MyWeather
 {
@@ -13,18 +13,28 @@ namespace MyWeather
     {
         private const string APIKEY = "e487ce3cd777104bda644f483932393c"; // private static readonly
         
-        private const string UNITS = "&units=metric"; // UNITS: imperial ("&units=imperial");  metric ("&units=metric");  standard (""); 
-        private const string LANGUAGE = "&lang=ru";
+        private const string UNITS = "metric"; // UNITS: imperial ("&units=imperial");  metric ("&units=metric");  standard (""); 
+        private const string LANGUAGE = "ru";
 
-        public string city = "London"; // using nowhere
+        [SerializeField]
+        private CityButton[] cityButtons;
+
+        private string url;
+        private string responseJson;
 
         public CityInfo[] cities;
 
         public WeatherResponse resp;
 
+        private DataTable tableFindCity;
+
+
         void Start()
         {
-            
+            // Check Network
+
+            //resp = GetWeather(); // Проверить, что будет если не будет интернета, посмотреть на сайте
+            resp = GetWeather(5174);
         }
 
         private WeatherResponse GetWeatherFromFile(string path)
@@ -32,35 +42,75 @@ namespace MyWeather
             string weather = File.ReadAllText(path); // check existing file path
 
             return JsonUtility.FromJson<WeatherResponse>(weather);
-        }
+        } // I think this no need more
 
-        private WeatherResponse GetWeather() // string city
+        private WeatherResponse GetWeather(string cityName) 
         {
-            string url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKEY + LANGUAGE + UNITS;
+            url = $"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIKEY}&lang={LANGUAGE}&units={UNITS}";
 
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
 
             HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
 
-            string response;
+            using (StreamReader streamReader = new StreamReader(webResponse.GetResponseStream()))
+            {
+                responseJson = streamReader.ReadToEnd();
+            }
+
+            //SaveWeatherInJSON(responseJson, "tempCity");
+
+            return JsonUtility.FromJson<WeatherResponse>(responseJson);
+        }
+
+        private WeatherResponse GetWeather(int cityId) 
+        {
+            url = $"http://api.openweathermap.org/data/2.5/weather?id={cityId}&appid={APIKEY}&lang={LANGUAGE}&units={UNITS}";
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
 
             using (StreamReader streamReader = new StreamReader(webResponse.GetResponseStream()))
             {
-                response = streamReader.ReadToEnd();
+                responseJson = streamReader.ReadToEnd();
             }
 
-            SaveWeatherInJSON(response, "tempCity");
+            return JsonUtility.FromJson<WeatherResponse>(responseJson);
+        }
 
-            return JsonUtility.FromJson<WeatherResponse>(response);
+        public void FindCity(string cityName) // Find City
+        {
+            HideCityButtons();
+
+            tableFindCity = SqliteDataAccess.GetTable("SELECT * " +
+                "FROM City " +
+                $"WHERE name like  '%{cityName}%' " + // ORDER BY name
+                "LIMIT 10");
+            Debug.Log(tableFindCity.Rows.Count);
+            ShowCityButtons(tableFindCity);
+        }
+
+        private void ShowCityButtons(DataTable tableCity)
+        {
+            for (int i = 0; i < tableCity.Rows.Count; i++)
+            {
+                cityButtons[i].Show(Convert.ToInt32(tableCity.Rows[i][0].ToString()), tableCity.Rows[i][1].ToString()); // Rows[i]["id_city"], Rows[i]["Name"]
+            }
+        }
+
+        private void HideCityButtons()
+        {
+            for (int i = 0; i < cityButtons.Length; i++)
+                cityButtons[i].Hide();
         }
 
         void SaveWeatherInJSON(string weather, string path)
         {
             Debug.Log("file Saved = " + Application.dataPath + @"/" + path + ".json");
             File.WriteAllText(Application.dataPath + @"/" + path + ".json", weather);
-        }
+        } // I think this no need more
 
-        void GetCitiesInfo()
+        void GetCitiesInfoFromFile()
         {
 
             string citiesInfoJsonPath = Application.streamingAssetsPath + @"\city.list.json"; // Move
@@ -69,7 +119,8 @@ namespace MyWeather
             cities = JsonHelper.FromJson<CityInfo>(citiesInfoJson);
             Debug.Log("UpdateCitiesInfo");
         }
-        void SaveCitiesInfo()
+
+        void SaveCitiesInfoInDB()
         {
             string query;
 
