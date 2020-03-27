2a5232fbb5ff23c9c62046f6739c62c8e42deac2
Dmitry <EncodingDeus@users.noreply.github.com>
2020-03-27 16:54:50 +0500

    All in heap

---
+++ Assets/Scripts/5DaysForecast/WeatherForecastResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyWeather {
+    [Serializable]
+    public class WeatherForecastResponse
+    {
+        public string cod;
+        public int message;
+        public int cnt;
+        public List<WeatherForecastInfo> list;
+        public CityInfo city;
+
+    }
+}
