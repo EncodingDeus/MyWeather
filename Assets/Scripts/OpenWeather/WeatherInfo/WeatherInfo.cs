68c127ba907ef4dee5b4646fd2263cbed7eab104
Dmitry <42086502+EncodingDeus@users.noreply.github.com>
2020-03-29 18:40:19 +0500

    Created constructor for WeatherForecastInfo and WeatherInfo

--- Assets/Scripts/OpenWeather/WeatherInfo/WeatherInfo.cs
+++ Assets/Scripts/OpenWeather/WeatherInfo/WeatherInfo.cs
@@ -5,6 +5,13 @@ namespace MyWeather
     [Serializable]
     public class WeatherInfo
     {
+        public WeatherInfo(string main, string description, string icon)
+        {
+            this.main = main;
+            this.description = description;
+            this.icon = icon;
+        }
+
         public int id;
         public string main;
         public string description;
